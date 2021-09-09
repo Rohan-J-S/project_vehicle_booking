5421cf78be7a88306cd220b45163dd7a9a3695a6
Rohan <53927306+Rohan-J-S@users.noreply.github.com>
2021-09-09 12:04:03 +0530

    Add files via upload

--- project.py
+++ project.py
@@ -4,18 +4,19 @@ c = base.cursor()
 from random import randint
 import pickle
 
+c.execute("drop table service_providers")
+c.execute("""create table service_providers( 
+    code int,
+    driver_name varchar(50),
+    vehicle_code int,
+   name varchar(50) ,
+    number int,
+   start_time int,
+   end_time int
 
-# c.execute("""create table service_providers( 
-#     code int,
-#     code_name varchar(50),
-#    name varchar(50) ,
-#     number int,
-#    start_time int,
-#    end_time int
+)
+""")
 
-# )
-# """)
-# (5,2) parameters total digits and number of digits after decimal
 def service_providers():
     l = []
     f1 = open("service_providers.dat" , 'ab')
@@ -37,24 +38,38 @@ def service_providers():
     f1.close()
 
     types = int(input("enter number of car models: "))
+    vehicle_code_1 = 0
     for x in range(types):
+        vehicle_code_1 += 1
             
         name_1 = input("enter car name: ")
         number_1 = int(input("enter number of units: "))
         start_time_1 = int(input("enter start time: "))
         end_time_1 = int(input("enter end time: "))
         others_1 = input("enter any other information: ")
-        c.execute("insert into service_providers(code , code_name , name,number, start_time , end_time) values({}, '{}', '{}' , {} , {} , {})".format(code_1 , code_name_1 , name_1 ,number_1, start_time_1 , end_time_1)) #syntax for user input insert into table
+        c.execute("insert into service_providers(code , driver_name ,vehicle_code, name,number, start_time , end_time) values({}, '{}', '{}' , {} , {} , {})".format(code_1 , code_name_1 ,vehicle_code_1 name_1 ,number_1, start_time_1 , end_time_1)) #syntax for user input insert into table
 
     base.commit()
 
 def customers():
-    print("code                 name                 car model        units available   start time        end time")
+    print("code                 name                   vehicle code                   car model        units available   start time        end time")
     data = c.execute("select * from service_providers")
     for x in data.fetchall():
         for y in x:
             print(y , end = "                 ")
         print()
+    
+    code = int(input("enter preffered service provider code from the table (0 to abort booking): "))
+    data = c.execute("select code from service_providers")
+    for x in data.fetchall():
+        
+        while code not in x:
+            print("code doesnt exist")
+            code = int(input("enter preffered service provider code from the table (0 to abort booking): "))
+        
+        print(".....")
+        vehicle_code = int(input("enter vehicle code: "))
+
     
 
 
